c55c679233dbb0102f4888b4c8039dd630373a19
Paw <paw@pawrequest.com>
2025-10-22 22:17:46 +0100

    ident strs

--- src/royal_mail_click_and_drop/v2/one_shots.py
+++ src/royal_mail_click_and_drop/v2/one_shots.py
@@ -17,5 +17,5 @@ def get_client():
 def cancel_order(ident: str):
     client = get_client()
     res = client.cancel_shipment(ident)
-    res_idents = res.deleted_order_idents
+    res_idents = res.idents
     print(res_idents)
