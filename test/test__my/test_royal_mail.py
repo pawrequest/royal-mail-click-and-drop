2f48f047a8be35240e51195191782aa495eaee84
Paw <paw@pawrequest.com>
2025-10-22 22:18:31 +0100

    ident strs

--- test/test__my/test_royal_mail.py
+++ test/test__my/test_royal_mail.py
@@ -27,14 +27,11 @@ def sample_client(sample_settings) -> Generator[RoyalMailClient, None, None]:
     yield client
 
     print('Deleting Test Orders')
-
     orders_after: GetOrdersResponse = client.fetch_orders()
     for o in orders_after.orders:
         if o not in orders_before.orders:
             res = client.cancel_shipment(order_ident=o.order_identifier)
-            assert o.order_identifier in [_.order_identifier for _ in res.deleted_orders], (
-                'WARNING, FAILED TO DELETE TEST ORDERS!!'
-            )
+            assert o.order_identifier in res.idents, 'WARNING, FAILED TO DELETE TEST ORDERS!!'
             print('Deleted Test Orders')
 
 
@@ -45,9 +42,13 @@ def sample_booking_response(sample_client, sample_orders) -> CreateOrdersRespons
 
 def test_book_order(sample_booking_response):
     order_identifier = sample_booking_response.created_orders[0].order_identifier
-    order_ident2 = sample_booking_response.created_orders_idents
     assert order_identifier
-    assert order_ident2 == str(order_identifier)
+
+
+def test_created_order_ident_str(sample_booking_response):
+    order_identifier = sample_booking_response.created_orders[0].order_identifier
+    assert order_identifier == sample_booking_response.created_orders_idents[0]
+    assert str(order_identifier) == sample_booking_response.created_orders_idents_str
 
 
 # def test_get_label(sample_client, sample_booking_response, tmp_path):
