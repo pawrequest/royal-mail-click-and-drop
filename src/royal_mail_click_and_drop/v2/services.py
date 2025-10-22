37c22477648372f93a946b53d78cbc9a34e6ac87
Paw <paw@pawrequest.com>
2025-10-22 13:32:57 +0100

    edit RoyalMailServiceCode

--- src/royal_mail_click_and_drop/v2/services.py
+++ src/royal_mail_click_and_drop/v2/services.py
@@ -2,5 +2,10 @@ from enum import StrEnum
 
 
 class RoyalMailServiceCode(StrEnum):
-    NEXT_DAY = 'TOLP24'  # no signature... use 'TOLP24SF' for signature
-    NEXT_DAY_12 = 'SD1OLP'  # £750 comp... use 'SD2OLP' for 1,000 or 'SD3OLP' for 2,500
+    TRACKED_24 = 'TOLP24'  # no signature.
+    TRACKED_24_SIGNED = 'TOLP24SF'
+    SPECIAL_PRE_12 = 'SD1OLP'  # £750 comp... use 'SD2OLP' for 1,000 or 'SD3OLP' for 2,500
+    EXPRESS_24 = 'PFE24'
+    EXPRESS_24_PRE_10 = 'PFE10'
+
+
