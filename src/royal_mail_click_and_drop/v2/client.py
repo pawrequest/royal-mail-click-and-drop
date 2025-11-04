5210eb115c1c8b5cacf6f5de93c35368ce981178
Paw <paw@pawrequest.com>
2025-11-04 17:41:33 +0000

    rm no-op error handling, add move chanel-shipper-only methods to superclass

--- src/royal_mail_click_and_drop/v2/client.py
+++ src/royal_mail_click_and_drop/v2/client.py
@@ -9,6 +9,8 @@ from royal_mail_click_and_drop import (
     CreateOrdersRequest,
     CreateOrdersResponse,
     DeleteOrdersResource,
+    GetOrderDetailsResource,
+    GetOrdersDetailsResponse,
     GetOrdersResponse,
     LabelsApi,
     ManifestOrdersResponse,
@@ -49,38 +51,22 @@ class RoyalMailClient(RMBaseModel):
         ident = str(order_ident)
         with ApiClient(self.config) as client:
             api = OrdersApi(client)
-            try:
-                response = api.delete_orders_async(order_identifiers=ident)
-            except ApiException as e:
-                print(f'Exception when calling OrdersApi->delete_orders_async: {e}\n')
-                raise
-        return response
+            return api.delete_orders_async(order_identifiers=ident)
 
-    def fetch_orders(self):
+    def fetch_orders(self) -> GetOrdersResponse:
         with ApiClient(self.config) as client:
             api = OrdersApi(client)
-            try:
-                response: GetOrdersResponse = api.get_orders_async()
-                pprint(response.model_dump(), indent=4, width=120)
-            except ApiException as e:
-                print(f'Exception when calling OrdersApi->delete_orders_async: {e}\n')
-                raise
-        return response
+            return api.get_orders_async()
 
-    def get_label_content(self, order_idents: str):
+    def get_label_content(self, order_idents: str) -> bytearray:
         with ApiClient(self.config) as client:
             api = LabelsApi(client)
-            try:
-                response: bytearray = api.get_orders_label_async(
-                    order_identifiers=order_idents,
-                    document_type='postageLabel',
-                    include_returns_label=False,
-                    include_cn=False,
-                )
-            except ApiException as e:
-                print(f'Exception when calling LabelsApi->get_orders_label_async: {e}\n')
-                raise
-        return response
+            return api.get_orders_label_async(
+                order_identifiers=order_idents,
+                document_type='postageLabel',
+                include_returns_label=False,
+                include_cn=False,
+            )
 
     def get_save_label(self, order_idents: str, outpath) -> bytearray:
         response = self.get_label_content(order_idents)
@@ -92,26 +78,44 @@ class RoyalMailClient(RMBaseModel):
         with ApiClient(self.config) as client:
             api = ManifestsApi(client)
             resp: ManifestOrdersResponse = api.manifest_eligible_async()
-            mainfest_num = resp.manifest_number
-            print(f'Manifested Orders, fetched Manifest Number: {mainfest_num}')
-            return resp
+        mainfest_num = resp.manifest_number
+        print(f'Manifested Orders, fetched Manifest Number: {mainfest_num}')
+        return resp
+
+    def retry_do_manifest(self, manifest_ident: int) -> ManifestOrdersResponse:
+        with ApiClient(self.config) as client:
+            api = ManifestsApi(client)
+            resp: ManifestOrdersResponse = api.retry_manifest_async(manifest_identifier=manifest_ident)
+        mainfest_num = resp.manifest_number
+        print(f'Retried Manifested Orders, fetched Manifest Number: {mainfest_num}')
+        return resp
 
     def fetch_version(self):
         with ApiClient(self.config) as client:
             api = VersionApi(client)
-            try:
-                response = api.get_version_async_with_http_info()
-                pprint(response.model_dump(), indent=4)
-            except ApiException:
-                print('ERROR')
+            response = api.get_version_async_with_http_info()
+        pprint(response.model_dump(), indent=4)
         return response
 
-    def cancel_all_orders(self, really=False) -> DeleteOrdersResource:
-        """Cancels ALL orders on the system - use with care / must pass really-True to work"""
-        if not really:
-            raise ValueError('Not cancelling orders, pass really=True to cancel')
-        res = self.fetch_orders()
-        if res.order_ident_string:
-            response = self.cancel_shipment(res.order_ident_string)
-            return response
-        raise ValueError('No order idents in response')
+    # def cancel_all_orders(self, really=False) -> DeleteOrdersResource:
+    #     """Cancels ALL orders on the system - use with care / must pass really-True to work"""
+    #     if not really:
+    #         raise ValueError('Not cancelling orders, pass really=True to cancel')
+    #     res = self.fetch_orders()
+    #     if res.order_ident_string:
+    #         response = self.cancel_shipment(res.order_ident_string)
+    #         return response
+    #     raise ValueError('No order idents in response')
+
+
+class RoyalMailChannelShipperClient(RoyalMailClient):
+    def fetch_specific_orders_with_details(self, order_ident: str | int) -> list[GetOrderDetailsResource]:
+        ident = str(order_ident)
+        with ApiClient(self.config) as client:
+            api = OrdersApi(client)
+            return api.get_specific_orders_with_details_async(order_identifiers=ident)
+
+    def fetch_orders_details(self) -> GetOrdersDetailsResponse:
+        with ApiClient(self.config) as client:
+            api = OrdersApi(client)
+            return api.get_orders_with_details_async()
