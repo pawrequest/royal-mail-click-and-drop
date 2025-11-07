56c7264f019b99307fa1a93d504c7796ca5b2eee
Paw <paw@pawrequest.com>
2025-11-07 17:18:37 +0000

    Add function to save and print manifest

--- src/royal_mail_click_and_drop/v2/one_shots.py
+++ src/royal_mail_click_and_drop/v2/one_shots.py
@@ -19,3 +19,8 @@ def cancel_order(ident: str):
     res = client.cancel_shipment(ident)
     res_idents = res.idents
     print(res_idents)
+
+
+def do_save_print_manifest():
+    client = get_client()
+    client.do_print_save_manifest()
